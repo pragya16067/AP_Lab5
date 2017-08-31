68687f650f285a183f69a33606db28eb2b4124e4
pragya16067 <pragya16067@iiitd.ac.in>
2017-08-31 14:30:21 +0530

    Final
    Completed Lab

--- Lab5.java
+++ Lab5.java
@@ -1,4 +1,9 @@
+//NAME : PRAGYA PRAKASH
+//Roll No. : 2016067
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,9 +14,10 @@ import java.util.Random;
 import java.util.StringTokenizer;
 
 class BSTNode<T> {
+	
 	private T data;
-	private BSTNode<T> left;
-	private BSTNode<T> right;
+	protected BSTNode<T> left;
+	protected BSTNode<T> right;
 	
 	public BSTNode(T value){
 		this.data=value;
@@ -25,30 +31,14 @@ class BSTNode<T> {
         this.data = value;
     }
 
-    public BSTNode<T> getLeft() {
-        return left;
-    }
-
-    public void setLeft(BSTNode<T> left) {
-        this.left = left;
-    }
-
-    public BSTNode<T> getRight() {
-        return right;
-    }
-
-    public void setRight(BSTNode<T> right) {
-        this.right = right;
-    }
     
-    
-    //CORRECT THIS and make FOR EVERY OBJECT
-    
-    //@Override
-    public boolean equals(BSTNode<T> other) {
-    	if(this==null && other==null)
+    //To check if 2 nodes are equal or not
+    public boolean equals(BSTNode<T> node,BSTNode<T> other) {
+    	//System.out.println(this.getData());
+    	if(node==null && other==null)
     		return true;
-    	if(this.data==other.data && this.left.equals(other.left) && this.right.equals(other.right))
+    	
+    	if(node.data==other.data && this.equals(node.left,other.left) && this.equals(node.right,other.right))
     	{
     		return true;
     	}
@@ -60,24 +50,40 @@ class BSTNode<T> {
 }
 
 class BST <T extends Comparable<T>> {
-	private BSTNode<T> RootNode;
+	protected BSTNode<T> RootNode;
 	
 	public BST(T rootval) {
-		BSTNode<T> RootNode=new BSTNode<T>(rootval);
+		 RootNode=new BSTNode<T>(rootval);
 	}
 	
-	public void insert(BSTNode<T> root, T val) {
+	public BSTNode<T> insert(BSTNode<T> root, T val) {
 		if(root==null)
 		{
 			root=new BSTNode<T>(val);
 		}
-		else if(root.getData().compareTo(val) <= 0)
+		else if(val.compareTo(root.getData()) <= 0)
 		{
-			insert(root.getLeft(),val);
+		     root.left=insert(root.left,val);
+		     
 		}
 		else
 		{
-			insert(root.getRight(),val);
+			root.right=insert(root.right,val);
+		}
+		return root;
+		
+	}
+	
+	public void PrintInorder(BSTNode<T> curr) {
+		if(curr==null)
+		{
+			return;
+		}
+		else
+		{
+			PrintInorder(curr.left);
+			System.out.print(curr.getData()+" ");
+			PrintInorder(curr.right);
 		}
 	}
 	
@@ -88,23 +94,22 @@ class BST <T extends Comparable<T>> {
 		}
 		else
 		{
-			findCorrespondingStudent(curNode.getLeft(),pos);
-			pos=pos+1;
-			if(curNode.equals(RootNode))
+			pos=findCorrespondingStudent(curNode.left,pos);
+			if(curNode.equals(RootNode,curNode))
 			{
 				return pos;
 			}
-			findCorrespondingStudent(curNode.getRight(),pos);
+			else
+			{
+				pos=pos+1;
+			}
+			
+			pos=findCorrespondingStudent(curNode.right,pos);
 			return pos;
 		}
 		
 	}
-	
-	public BSTNode<T> getRoot() {
-		return this.RootNode;
-	}
-	
-	
+		
 	
 }
 
@@ -121,72 +126,84 @@ public class Lab5 {
 		
 		HashMap <Integer, ArrayList<Object>> H=new HashMap <Integer, ArrayList<Object>> ();
 		//create all keys as roll no of students
-		for(int i=1; i<KidsNo; i++)
+		for(int i=1; i<=KidsNo; i++)
 		{
-			H.put(i, new ArrayList<Object>() );
+			ArrayList<Object> a=new ArrayList<Object> ();
+			H.put(i, a);
 		}
 		
 		for(int i=1; i<=TreeNo; i++)
 		{
+			//Taking input from files
+			ReaderFromFile.init("./src/" + i + ".txt");
 			//build tree and assign to a kid
-			String type=Reader.reader.readLine();
+			String type=ReaderFromFile.reader.readLine();
 			//BST bst;
-			int n=Reader.nextInt(); //number of nodes in a tree
+			int n=ReaderFromFile.nextInt(); //number of nodes in a tree
 			//an object variable to store the answer of each tree
 			Object ans;
 			//an integer variable to store the position of the root in inorder traversal
 			int childNo=0;
 			if(type.equals("Integer"))
 			{
-				int root=Reader.nextInt();
+				int root=ReaderFromFile.nextInt();
 				ans=root;
 				BST<Integer> bst=new BST<Integer>(root);
-				for(int ctr=0; ctr<n; ctr++)
+				for(int ctr=1; ctr<n; ctr++)
 				{
-					int val=Reader.nextInt();
-					bst.insert(bst.getRoot(), val);
+					int val=ReaderFromFile.nextInt();
+					bst.insert(bst.RootNode, val);
 					ans=(int) ans + val;
 				}
-				childNo=bst.findCorrespondingStudent(bst.getRoot(), 0);
+				//bst.PrintInorder(bst.RootNode);
+				//System.out.println();
+				childNo=bst.findCorrespondingStudent(bst.RootNode, 1);
 			}
 			else if(type.equals("Float"))
 			{
-				float root=//read float from file
+				float root=ReaderFromFile.nextFloat();
 				BST<Float> bst=new BST<Float>(root);
 				ans= root;
-				for(int ctr=0; ctr<n; ctr++)
+				for(int ctr=1; ctr<n; ctr++)
 				{
-					float val=//read the float from file
-					bst.insert(bst.getRoot(), val);
+					float val=ReaderFromFile.nextFloat();         //read the float from file
+					bst.insert(bst.RootNode, val);
 					ans=(float) ans + val;
 				}
-				childNo=bst.findCorrespondingStudent(bst.getRoot(), 0);
+				//bst.PrintInorder(bst.RootNode);
+				childNo=bst.findCorrespondingStudent(bst.RootNode, 1);
 			}
 			else
 			{
-				String s=Reader.reader.readLine();
-				String[] A=s.split(" ");
-				BST<String> bst=new BST<String> (A[0]);
-				ans=A[0];
+				String root=ReaderFromFile.next();
+				
+				BST<String> bst=new BST<String> (root);
+				ans=root;
 				for(int ctr=1; ctr<n; ctr++)
 				{
-					bst.insert(bst.getRoot(), A[1]);
-					ans=ans+A[i];
+					String s=ReaderFromFile.next();
+					bst.insert(bst.RootNode, s);
+					ans=ans+s;
 				}
-				childNo=bst.findCorrespondingStudent(bst.getRoot(), 0);
+				//bst.PrintInorder(bst.RootNode);
+				childNo=bst.findCorrespondingStudent(bst.RootNode, 1);
 			}
 			
 			//insert the tree's answer in the Arraylist for that particular child
-			ArrayList<Object> o=H.get(childNo);
+			//System.out.println(childNo);
 			
-			o.add(ans); //Will this add to the arraylist on hashmap too????
+			ArrayList<Object> o = H.get(childNo);
+			o.add(ans); 
+			
 			
 		}
 		
 		int chocoKids=0; //to get the number of kids getting a chocolate
 		
 		//now for final output display
-		for(int k=1; k<KidsNo; k++)
+		
+		PrintWriter OPwrite = new PrintWriter("./src/Output.txt", "UTF-8");
+		for(int k=1; k<=KidsNo; k++)
 		{
 			ArrayList<Object> o=H.get(k);
 			if(o.size()==0)
@@ -195,15 +212,16 @@ public class Lab5 {
 			}
 			else
 			{
-				System.out.print(k+" ");
+				OPwrite.print(k+" ");
 				for(int i=0; i<o.size(); i++)
 				{
-					System.out.print(o.get(i)+" ");
+					OPwrite.print(o.get(i)+" ");
 				}
-				System.out.println();
+				OPwrite.println();
 			}
 		}
-		System.out.println(chocoKids);
+		OPwrite.println(chocoKids);
+		OPwrite.close();
 		
 	}
 	
@@ -277,6 +295,50 @@ class Reader {
 
     static int nextInt() throws IOException {
         return Integer.parseInt( next() );
+    }
+    
+    static float nextFloat() throws IOException {
+        return Float.parseFloat( next() );
+    }
+    
+    static long nextLong() throws IOException {
+        return Long.parseLong( next() );
+    }
+	
+    static double nextDouble() throws IOException {
+        return Double.parseDouble( next() );
+    }
+}
+
+/** Class for buffered reading int and double values */
+class ReaderFromFile {
+    static BufferedReader reader;
+    static StringTokenizer tokenizer;
+
+    /** call this method to initialize reader for InputStream 
+     * @throws FileNotFoundException */
+    static void init(String input) throws FileNotFoundException {
+        reader = new BufferedReader(
+                     new FileReader(input) );
+        tokenizer = new StringTokenizer("");
+    }
+
+    /** get next word */
+    static String next() throws IOException {
+        while ( ! tokenizer.hasMoreTokens() ) {
+            //TODO add check for eof if necessary
+            tokenizer = new StringTokenizer(
+                   reader.readLine() );
+        }
+        return tokenizer.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt( next() );
+    }
+    
+    static float nextFloat() throws IOException {
+        return Float.parseFloat( next() );
     }
     
     static long nextLong() throws IOException {
